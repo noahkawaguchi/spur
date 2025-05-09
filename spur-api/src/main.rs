aa40734a8174fe995b2b41f218699aa463df0ce8
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-05-09 16:20:17 -0400

    Initial dependencies and hello world server

--- spur-api/src/main.rs
+++ spur-api/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+const ADDRESS: &str = "127.0.0.1:3000";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(hello_world));
+    let listener = TcpListener::bind(ADDRESS).await?;
+
+    println!("Listening on http://{}...", ADDRESS);
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
+
+async fn hello_world() -> &'static str { "Hello, World!" }
