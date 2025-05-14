878794639fc5cceb6104696ec9b34eb744a34bd9
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-05-13 20:05:52 -0400

    Write tests for JWT service

--- spur-api/src/services/jwt_svc.rs
+++ spur-api/src/services/jwt_svc.rs
@@ -45,3 +45,65 @@ pub fn verify_jwt(token: &str, secret: &[u8]) -> Result<i32> {
 
     Ok(id)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod claims {
+        use super::*;
+        use chrono::{DateTime, Days};
+
+        #[test]
+        fn converts_types_and_calculates_expiration() {
+            let id = 825;
+            let tomorrow = Utc::now()
+                .checked_add_days(Days::new(1))
+                .expect("failed to compute tomorrow");
+
+            let claims = Claims::new(id).expect("failed to create claims");
+
+            let exp = DateTime::from_timestamp(
+                claims.exp.try_into().expect("failed to convert u64 to i64"),
+                0,
+            )
+            .expect("failed to create datetime");
+
+            assert!((exp - tomorrow).num_seconds().abs() <= 1);
+            assert_eq!(claims.sub, id.to_string());
+        }
+    }
+
+    mod tokens {
+        use super::*;
+
+        #[test]
+        fn encodes_and_decodes() {
+            let id = 783;
+            let secret = "super_duper_secret".as_ref();
+
+            let token = create_jwt(id, secret).expect("failed to create token");
+            let got_id = verify_jwt(&token, secret).expect("failed to verify token");
+
+            assert_eq!(got_id, id);
+        }
+
+        #[test]
+        fn errors_for_invalid_token() {
+            let id = 4621;
+            let secret = "no one's gonna know".as_ref();
+
+            let _ = create_jwt(id, secret).expect("failed to create token");
+            assert!(verify_jwt("fake token", secret).is_err());
+        }
+
+        #[test]
+        fn errors_for_invalid_secret() {
+            let id = 999;
+            let secret = "shh".as_ref();
+
+            let token = create_jwt(id, secret).expect("failed to create token");
+            assert!(verify_jwt(&token, "boo!".as_ref()).is_err());
+        }
+    }
+}
