f4167976220a99314886e8ca3aadc66ede3ec674
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-07-24 16:32:02 -0400

    Use `Link` and `table` in `FriendsPage`

--- frontend/src/pages/Friends/FriendsPage.tsx
+++ frontend/src/pages/Friends/FriendsPage.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
 import { useTokenOrRedirect } from '../../utils/jwt';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useRequest from '../../hooks/useRequest';
 import { UsernamesResponseSchema, type UsernamesResponse } from '../../types';
 
 const FriendsPage = () => {
   const token = useTokenOrRedirect();
-  const navigate = useNavigate();
 
   const { data, error, loading, sendRequest } = useRequest<UsernamesResponse>(
     'GET',
@@ -21,31 +20,36 @@ const FriendsPage = () => {
   return (
     <>
       <h2>Friends</h2>
-      <button type='button' onClick={() => void navigate('/friends/requests')}>
-        See pending requests
-      </button>
-      <button type='button' onClick={() => void navigate('/friends/add')}>
-        Add a new friend
-      </button>
+      <Link to='/friends/requests'>
+        <button type='button'>See pending requests</button>
+      </Link>
+      <Link to='/friends/add'>
+        <button type='button'>Add a new friend</button>
+      </Link>
       <hr />
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      {data &&
-        (data.usernames.length ? (
-          <>
-            <h3>Current friends</h3>
-            {data.usernames.map(username => (
-              <div key={username}>
-                {username}{' '}
-                <Link to={`/friends/${username}`}>
-                  <button type='button'>View profile</button>
-                </Link>{' '}
-              </div>
-            ))}
-          </>
-        ) : (
-          <p>(No friends)</p>
-        ))}
+      {data && (
+        <>
+          <h3>Current friends</h3>
+          {data.usernames.length ? (
+            <table>
+              <tbody>
+                {data.usernames.map(username => (
+                  <tr key={username}>
+                    <td>{username}</td>
+                    <Link to={`/friends/${username}`}>
+                      <button type='button'>View profile</button>
+                    </Link>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>(No friends)</p>
+          )}
+        </>
+      )}
     </>
   );
 };
