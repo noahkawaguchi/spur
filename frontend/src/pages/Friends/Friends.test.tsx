ce8234c7d61061a8f59aa737a73e495e2416d8ce
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-09-23 15:15:18 -0400

    Write tests for `FriendsPage`

---
+++ frontend/src/pages/Friends/Friends.test.tsx
@@ -0,0 +1,34 @@
+import FriendsPage from '@/pages/Friends';
+import { inMemRouter } from '@/test-utils/router';
+import { initMockUseRequestResult } from '@/test-utils/types';
+import { render, screen } from '@testing-library/react';
+
+const mockUseRequestResultState = initMockUseRequestResult<string[]>();
+const mockToken = 'this is a mock token';
+
+const mocks = vi.hoisted(() => {
+  return {
+    mockUseRequest: vi.fn(() => mockUseRequestResultState),
+    mockUseTokenOrRedirect: vi.fn(() => mockToken),
+  };
+});
+
+vi.mock('@/hooks/useRequest', () => ({ default: mocks.mockUseRequest }));
+vi.mock('@/hooks/useTokenOrRedirect', () => ({ default: mocks.mockUseTokenOrRedirect }));
+
+describe('FriendsPage', () => {
+  afterEach(() => vi.clearAllMocks());
+
+  it('should display a no friends message if the usernames array is empty', () => {
+    mockUseRequestResultState.data = [];
+    render(inMemRouter({ children: <FriendsPage /> }));
+    expect(screen.getByText('(No friends)')).toBeInTheDocument();
+  });
+
+  it('should display friend usernames if they exist', () => {
+    const friends = ['jeff1', 'jeff2', 'jefe', 'jpeg'];
+    mockUseRequestResultState.data = friends;
+    render(inMemRouter({ children: <FriendsPage /> }));
+    friends.forEach(friend => expect(screen.getByText(friend)).toBeInTheDocument());
+  });
+});
