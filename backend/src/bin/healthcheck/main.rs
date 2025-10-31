cf31838e4ccf0a1091a11df3c37b71e7b22ab972
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-10-30 20:48:00 -0400

    Add `migrate` and `healthcheck` binaries [#30]

---
+++ backend/src/bin/healthcheck/main.rs
@@ -0,0 +1,30 @@
+use std::{
+    env,
+    error::Error,
+    net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream},
+    time::Duration,
+};
+
+/// Makes a very lightweight attempt to connect to the main server over localhost as a health check.
+/// Gets the port from the same `BIND_ADDR` environment variable that the server uses. Waits for 300
+/// ms unless a different value is provided via the environment variable `HEALTHCHECK_MILLIS`.
+fn main() -> Result<(), Box<dyn Error>> {
+    let port = env::var("BIND_ADDR")?
+        .split_once(':')
+        .ok_or("failed to split BIND_ADDR on ':'")?
+        .1
+        .parse()?;
+
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+
+    let timeout = Duration::from_millis(
+        env::var("HEALTHCHECK_MILLIS")
+            .ok()
+            .and_then(|val| val.parse().ok())
+            .unwrap_or(300),
+    );
+
+    TcpStream::connect_timeout(&socket, timeout)?;
+
+    Ok(())
+}
---
+++ backend/src/bin/migrate/main.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use sqlx::{migrate::Migrator, postgres::PgPoolOptions};
+
+/// Database migrator that embeds migrations into the binary at compile time.
+static MIGRATOR: Migrator = sqlx::migrate!();
+
+/// Connects to the database specified in the environment variable `DATABASE_URL` and validates the
+/// migrations embedded into the binary, applying any that are pending.
+#[tokio::main]
+async fn main() -> Result<()> {
+    spur::logger::init_with_default(log::LevelFilter::Info);
+    log::info!("Starting database migrations...");
+
+    let pool = PgPoolOptions::new()
+        .connect(&std::env::var("DATABASE_URL")?)
+        .await?;
+
+    log::info!("Connected to database");
+
+    MIGRATOR.run(&pool).await?;
+    log::info!("Migrations applied/validated");
+
+    Ok(())
+}
